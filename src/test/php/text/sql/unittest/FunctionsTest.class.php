fa539b8f04fa06ae6231c27e3fde777e8f457bdb
Timm Friebe <thekid@thekid.de>
2020-11-06 23:08:20 +0100

    Verify spaces after function call names work as expected

--- src/test/php/text/sql/unittest/FunctionsTest.class.php
+++ src/test/php/text/sql/unittest/FunctionsTest.class.php
@@ -7,6 +7,7 @@ class FunctionsTest extends StatementTest {
   /** @return iterable */
   protected function statements() {
     yield ['select now()', new Select([new Call('now', [])])];
+    yield ['select now ()', new Select([new Call('now', [])])];
     yield ['select mod(29, 2)', new Select([new Call('mod', [new Number(29), new Number(2)])])];
     yield ['select count(*) from user', new Select([new Call('count', [new All()])], [new Table('user')])];
     yield ['select count(u.*) from user u', new Select([new Call('count', [new All('u')])], [new Table('user', 'u')])];
